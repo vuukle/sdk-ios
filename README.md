39174e38c7647527bd0e330f7613895eb2f393cf
Narek <narek1994t@gmail.com>
2021-01-06 13:36:59 +0400

    - Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sdk-ios
 Vuukle iOS SDK
+
---
+++ VukkleExample/GlobalConfig.swift
@@ -0,0 +1,49 @@
+//
+//  GlobalConfig.swift
+//  VukkleExample
+//
+//  Created by Narek Dallakyan on 11/19/20.
+//  Copyright © 2020 MAC_7. All rights reserved.
+//
+
+import Foundation
+
+let VUUKLE_IFRAME = "https://cdn.vuukle.com/amp.html?url=https%3A%2F%2Fwww.prowrestling.com%2Fimpact-wrestling-results-1282020%2F&host=prowrestling.com&id=1196371&apiKey=46489985-43ef-48ed-9bfd-61971e6af217&img=https%3A%2F%2Fwww.prowrestling.com%2Fwp-content%2Fuploads%2F2020%2F12%2FKenny-Omega-Impact-Wrestling.jpeg&title=IMPACT%2BWrestling%2BResults%2B%252812%252F8%2529%253A%2BKenny%2BOmega%2BSpeaks%252C%2BKnockouts%2BTag%2BTournament%2BContinues%2521&tags=Featured"
+
+let VUUKLE_EMOTES = "https://cdn.vuukle.com/widgets/emotes.html?apiKey=c7368a34-dac3-4f39-9b7c-b8ac2a2da575&darkMode=false&host=smalltester.000webhostapp.com&articleId=381&img=https://smalltester.000webhostapp.com/wp-content/uploads/2017/10/wallhaven-303371-825x510.jpg&title=New&post&22&url=https://smalltester.000webhostapp.com/2017/12/new-post-22&emotesEnabled=true&firstImg=&secondImg=&thirdImg=&fourthImg=&fifthImg=&sixthImg=&totWideImg=false&articlesProtocol=http&hideArticles=false&disable=[]&iconsSize=70&first=HAPPY&second=INDIFFERENT&third=AMUSED&fourth=EXCITED&fifth=ANGRY&sixth=SAD&customText=%7B%7D"
+
+let VUUKLE_POWERBAR = "https://cdntest.vuukle.com/widgets/powerbar.html?amp=false&apiKey=664e0b85-5b2c-4881-ba64-3aa9f992d01c&host=relaxed-beaver-76304e.netlify.com&articleId=Index&img=https%3A%2F%2Fwww.gettyimages.ie%2Fgi-resources%2Fimages%2FHomepage%2FHero%2FUK%2FCMS_Creative_164657191_Kingfisher.jpg&title=Index&url=https%3A%2F%2Frelaxed-beaver-76304e.netlify.app%2F&tags=123&author=123&lang=en&gr=false&darkMode=false&defaultEmote=1&items=&standalone=0&mode=horizontal"
+
+// Social Login URLs
+let VUUKLE_SOCIAL_LOGIN = "https://login.vuukle.com/auth"
+let VUUKLE_SOCIAL_LOGIN_SUCCESS = "https://login.vuukle.com/consent"
+
+// Settings Url
+let VUUKLE_SETTINGS = "https://news.vuukle.com/settings/edit-profile"
+
+// Soccial Share
+let VUUKLE_FB_SHARE = "https://www.facebook.com/share"
+let VUUKLE_TWITTER_SHARE = "https://twitter.com/share"
+let VUUKLE_WHATSAPP_SHARE = "https://api.whatsapp.com/"
+let VUUKLE_WEB_WHATSAPP_SHARE = "https://web.whatsapp.com/send"
+let VUUKLE_TELEGRAM_SHARE = "https://telegram.me/share"
+let VUUKLE_LINKEDIN_SHARE = "https://www.linkedin.com/shareArticle"
+let VUUKLE_REDDIT_SHARE = "https://www.reddit.com"
+let VUUKLE_PINTEREST_SHARE = "https://pinterest.com"
+let VUUKLE_FLIPBOARD_SHARE  = "https://share.flipboard.com"
+let VUUKLE_TUMBLR_SHARE  = "https://tumblr.com/"
+let VUUKLE_MAIL_SHARE = "mailto:?subject"
+let VUUKLE_MAIL_TO_SHARE = "mailto:to?subject"
+let VUUKLE_MESSENGER_SHARE = "fb-messenger://share"
+
+// Vuukle News
+let VUUKLE_NEWS_BASE_URL = "https://news.vuukle.com/"
+// Vuukle Privacy
+let VUUKLE_PRIVACY = "https://docs.vuukle.com/privacy-and-policy/"
+// Vuukle notification base URL
+let VUUKLE_NOTIFICATION_BASE = "https://api.vuukle.com"
+// Vuukle Base URL
+let VUUKLE_BASE = "https://vuukle.com/"
+let VUUKLE_IFRAME_BASE = "https://cdn.vuukle.com"
+
+let VUUKLE_URLS = [VUUKLE_NEWS_BASE_URL, VUUKLE_PRIVACY, VUUKLE_NOTIFICATION_BASE, VUUKLE_BASE, VUUKLE_MESSENGER_SHARE, VUUKLE_MAIL_SHARE, VUUKLE_TUMBLR_SHARE, VUUKLE_FLIPBOARD_SHARE, VUUKLE_PINTEREST_SHARE, VUUKLE_REDDIT_SHARE, VUUKLE_LINKEDIN_SHARE, VUUKLE_TELEGRAM_SHARE, VUUKLE_WEB_WHATSAPP_SHARE, VUUKLE_WHATSAPP_SHARE, VUUKLE_TWITTER_SHARE, VUUKLE_FB_SHARE, VUUKLE_SOCIAL_LOGIN, VUUKLE_SOCIAL_LOGIN_SUCCESS]
